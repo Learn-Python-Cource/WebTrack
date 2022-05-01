3dee8d3074320bac7d5a2d3f6f352227b6fa26d3
Zorkin Roman <zorkin@astra.home.net>
2022-05-01 08:23:59 +0700

    Implement user model

--- service/models.py
+++ service/models.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -10,5 +12,21 @@ class News(db.Model):
     published = db.Column(db.DateTime, nullable=True)
     text = db.Column(db.Text, nullable=True)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column()
+    password = db.Column()
+    role = db.Column()
+
+    def set_password(self, password):
+        pass
+
+    def check_password(self, password):
+        pass
+
+    def __repr__(self) -> str:
+        return '<User {0} id={1}>'.format(self.username, self.id)
